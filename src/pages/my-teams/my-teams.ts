d822eea885f2060df50182c89888649046280e22
pashko.ostap <pashko.ostap@gmail.com>
2017-06-07 20:31:13 +0300

    updated my-teams page with list of favorite teams

--- src/pages/my-teams/my-teams.ts
+++ src/pages/my-teams/my-teams.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LoadingController, IonicPage, NavController, NavParams } from 'ionic-angular';
 
-import { TournamentsPage } from '../';
+import { TeamHomePage, TournamentsPage } from '../';
+import { ApiService } from '../../shared';
 
 @IonicPage()
 @Component({
@@ -9,10 +10,45 @@ import { TournamentsPage } from '../';
   templateUrl: 'my-teams.html',
 })
 export class MyTeamsPage {
+  favorites = [
+    {
+      team: {
+        id: 1,
+        name: 'Name 1',
+        coach: 'Coach 1'
+      },
+      tournamentId: 'id 1',
+      tournamentName: 'tournament name 1'
+    },
+    {
+      team: {
+        id: 2,
+        name: 'Name 2',
+        coach: 'Coach 2'
+      },
+      tournamentId: 'id 2',
+      tournamentName: 'tournament name 2'
+    }
+  ]
 
-  constructor(private navCtrl: NavController, public navParams: NavParams) { }
+  constructor(
+    private navCtrl: NavController,
+    public navParams: NavParams,
+    private apiService: ApiService,
+    private loadingController: LoadingController) { }
 
   goToTournament() {
     this.navCtrl.push(TournamentsPage);
+  }
+
+  favoriteTapped(e, favorite) {
+    let loader = this.loadingController.create({
+      content: 'Getting data...',
+      dismissOnPageChange: true
+    })
+    loader.present();
+    this.apiService.getTournamentsData(favorite.tournamentId).subscribe(team =>{
+      this.navCtrl.push(TeamHomePage, favorite.team);
+    })
   }
 }
