02e79558a7ac5b739599356f5a0db740c97b18e8
pashko.ostap <pashko.ostap@gmail.com>
2017-06-08 23:47:02 +0300

    updated standings page

--- src/pages/standings/standings.ts
+++ src/pages/standings/standings.ts
@@ -1,24 +1,31 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ApiService } from "../../shared/";
+import * as _ from 'lodash';
 
-/**
- * Generated class for the StandingsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-standings',
   templateUrl: 'standings.html',
 })
 export class StandingsPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
+  standings: any[];
+  team: any;
+  allStandings: any;
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private apiService: ApiService) { }
   ionViewDidLoad() {
-    console.log('ionViewDidLoad StandingsPage');
-  }
+    this.team = this.navParams.data;
+    let tourneyData = this.apiService.getCurrentTourney();
+    this.standings = tourneyData.standings;
 
+    this.allStandings = 
+      _.chain(this.standings)
+       .groupBy('division')
+       .toPairs()
+       .map(item => _.zipObject(['divisionName', 'divisionStandings'], item))
+       .value()
+  }
 }
